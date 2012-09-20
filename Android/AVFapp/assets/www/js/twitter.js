f3a07463bc6f519c46d63d31c943f72f22adaf81
Andrew Phillips <andrewtylerphillips@gmail.com>
2012-09-20 15:51:06 -0500

    updates applied for iOS/Android

--- Android/AVFapp/assets/www/js/twitter.js
+++ Android/AVFapp/assets/www/js/twitter.js
@@ -7,7 +7,7 @@
 $(function() {
 	$.getJSON("http://search.twitter.com/search.json?q=star%20wars&callback=?&rpp=3", function(crawl) {
 		console.log(crawl);
-		$("#crawl").html("<p>Episode III - Star Wars Tweets</p>");
+		$("#crawl").html("<p>Star Wars Twitter Crawl!</p>");
 
 		for (i=0, j=crawl.results.length; i<j; i++) {
 
