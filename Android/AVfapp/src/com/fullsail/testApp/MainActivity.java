4a419630d0cc9d68f1b7ba3de09298f66f36849f
Andrew Phillips <andrewtylerphillips@gmail.com>
2012-09-21 20:22:35 -0500

    final fixes - works on everything, sans actual iPad

--- Android/AVfapp/src/com/fullsail/testApp/MainActivity.java
+++ Android/AVfapp/src/com/fullsail/testApp/MainActivity.java
@@ -2,7 +2,6 @@ package com.fullsail.testApp;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
 import org.apache.cordova.*;
 
 public class MainActivity extends DroidGap {
@@ -10,12 +9,6 @@ public class MainActivity extends DroidGap {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.loadUrl("file:///android_asset/www/index.html");    setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
+        super.loadUrl("file:///android_asset/www/index.html");    
     }
 }
